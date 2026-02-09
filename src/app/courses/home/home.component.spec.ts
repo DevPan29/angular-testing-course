7153e4f4b17156835a58f077436e48f3568a7740
AlePan <darenagel@hotmail.it>
2026-02-09 13:51:08 +0100

    test with programmatic click

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -1,106 +1,110 @@
-import {waitForAsync, ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed} from '@angular/core/testing';
-import {CoursesModule} from '../courses.module';
-import {DebugElement} from '@angular/core';
+import {
+  waitForAsync,
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  flushMicrotasks,
+  TestBed,
+} from "@angular/core/testing";
+import { CoursesModule } from "../courses.module";
+import { DebugElement } from "@angular/core";
 
-import {HomeComponent} from './home.component';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
-import {CoursesService} from '../services/courses.service';
-import {HttpClient} from '@angular/common/http';
-import {COURSES} from '../../../../server/db-data';
-import {setupCourses} from '../common/setup-test-data';
-import {By} from '@angular/platform-browser';
-import {of} from 'rxjs';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {click} from '../common/test-utils';
+import { HomeComponent } from "./home.component";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { CoursesService } from "../services/courses.service";
+import { HttpClient } from "@angular/common/http";
+import { COURSES } from "../../../../server/db-data";
+import { setupCourses } from "../common/setup-test-data";
+import { By } from "@angular/platform-browser";
+import { of } from "rxjs";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { click } from "../common/test-utils";
 
-
-
-
-describe('HomeComponent', () => {
-
+describe("HomeComponent", () => {
   let fixture: ComponentFixture<HomeComponent>;
-  let component:HomeComponent;
+  let component: HomeComponent;
   let el: DebugElement;
   let coursesService: any;
-  const beginnnerCourses = setupCourses()
-    .filter(course => course.category === 'BEGINNER');
-  const advancedCourses = setupCourses()
-    .filter(course => course.category === 'ADVANCED');
+  const beginnnerCourses = setupCourses().filter(
+    (course) => course.category === "BEGINNER",
+  );
+  const advancedCourses = setupCourses().filter(
+    (course) => course.category === "ADVANCED",
+  );
 
   beforeEach(waitForAsync(() => {
-
-    const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses'])
+    const coursesServiceSpy = jasmine.createSpyObj("CoursesService", [
+      "findAllCourses",
+    ]);
 
     TestBed.configureTestingModule({
-      imports: [
-        CoursesModule,
-        NoopAnimationsModule
-      ],
-      providers: [
-        {provide: CoursesService, useValue: coursesServiceSpy}
-      ]
-    }).compileComponents().then(() => {
-      fixture = TestBed.createComponent(HomeComponent);
-      component = fixture.componentInstance;
-      el = fixture.debugElement;
-      coursesService = TestBed.inject(CoursesService);
+      imports: [CoursesModule, NoopAnimationsModule],
+      providers: [{ provide: CoursesService, useValue: coursesServiceSpy }],
     })
-
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+        coursesService = TestBed.inject(CoursesService);
+      });
   }));
 
   it("should create the component", () => {
-
     expect(component).toBeTruthy();
-
   });
 
-
   it("should display only beginner courses", () => {
-
     coursesService.findAllCourses.and.returnValue(of(beginnnerCourses));
-    fixture.detectChanges() 
+    fixture.detectChanges();
 
     // here the observable  beginnerCourses$ of HomeComponent should be resolved and the template should be updated with the beginner courses
 
     // old css - const tabs = el.queryAll(By.css(".mat-mdc-tab-label"))
-    const tabs = el.queryAll(By.css('.mdc-tab__text-label'));
+    const tabs = el.queryAll(By.css(".mdc-tab__text-label"));
 
     expect(tabs.length).toBe(1, "Unexpected number of tabs found");
-
   });
-
 
   it("should display only advanced courses", () => {
-
     coursesService.findAllCourses.and.returnValue(of(advancedCourses));
-    fixture.detectChanges() 
+    fixture.detectChanges();
 
-    const tabs = el.queryAll(By.css('.mdc-tab__text-label'));
+    const tabs = el.queryAll(By.css(".mdc-tab__text-label"));
 
     expect(tabs.length).toBe(1, "Unexpected number of tabs found");
-
   });
-
 
   it("should display both tabs", () => {
-
     coursesService.findAllCourses.and.returnValue(of(setupCourses()));
-    
-    fixture.detectChanges() 
 
-    const tabs = el.queryAll(By.css('.mdc-tab__text-label'));
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mdc-tab__text-label"));
 
     expect(tabs.length).toBe(2, "Unexpected number of tabs found");
-
-  }); 
-
-
-  it("should display advanced courses when tab clicked", () => {
-
-    pending();
-
   });
 
+  it("should display advanced courses when tab clicked", () => {
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mat-tab-label"));
+
+    click(tabs[1]);
+
+    fixture.detectChanges();
+
+    const cardTitles = el.queryAll(By.css(".mat-card-title"));
+
+    expect(cardTitles.length).toBe(6, "Unexpected number of courses found");
+
+    expect(cardTitles[0].nativeElement.textContent).toContain(
+      "Angular Security Course",
+    );
+  });
 });
-
-
--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,46 +1,37 @@
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-describe('CalculatorService', () => {
+describe("CalculatorService", () => {
+  let calculator: CalculatorService, loggerSpy: any;
 
-    let calculator: CalculatorService,
-        loggerSpy: any;
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
 
-    beforeEach(() => {
-
-        console.log('Before Each');
-        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
-
-        TestBed.configureTestingModule({
-            providers: [
-                CalculatorService,
-                { provide: LoggerService, useValue: loggerSpy }
-            ]
-        });
-
-        // OLD version - calculator = TestBed.get(CalculatorService);
-        calculator = TestBed.inject(CalculatorService);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy },
+      ],
     });
 
-    it('should add two numbers', () => {
+    // OLD version - calculator = TestBed.get(CalculatorService);
+    calculator = TestBed.inject(CalculatorService);
+  });
 
-        console.log('should add two numbers');
-        const result = calculator.add(2, 2);
+  it("should add two numbers", () => {
+    const result = calculator.add(2, 2);
 
-        expect(result).toBe(4);
+    expect(result).toBe(4);
 
-        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-    })
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
 
-    it('should subtract two numbers', () => {
+  it("should subtract two numbers", () => {
+    const result = calculator.subtract(2, 2);
 
-        console.log('should subtract two numbers');
-        const result = calculator.subtract(2, 2);
+    expect(result).toBe(0, "unexpected subtraction result");
 
-        expect(result).toBe(0, "unexpected subtraction result");
-
-        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-    })
-
-})
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+  });
+});
