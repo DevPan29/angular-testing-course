5b66d868c77bd25bc3328f72052af9fc8e713c1c
Alessandro Panarese <darenagel@hotmail.it>
2026-02-13 14:28:21 +0100

    Update async-example.spec.ts

--- src/app/courses/home/async-example.spec.ts
+++ src/app/courses/home/async-example.spec.ts
@@ -1,3 +1,5 @@
+import { fakeAsync, flush, flushMicrotasks, tick } from "@angular/core/testing";
+
 fdescribe("Async Testing Examples", () => {
     
     it("Asynchronous test example with Jasmine done", (done: DoneFn) => {
@@ -6,8 +8,6 @@ fdescribe("Async Testing Examples", () => {
 
         setTimeout(() => {
 
-            console.log("running assertions");
-
             test = true;
             expect(test).toBeTruthy();
 
@@ -15,8 +15,56 @@ fdescribe("Async Testing Examples", () => {
 
         }, 1000);
 
-        console.log("END of test");
-
     })
+
+    it("Asyncronous text example with setTimeout()", fakeAsync(() => {
+
+        let test = false;
+
+        setTimeout(() => {
+            console.log("running assertions 1");
+        })
+
+         setTimeout(() => {
+
+            console.log("running assertions 2");
+
+            test = true;
+            
+
+
+        }, 1000);
+
+        flush();
+        expect(test).toBeTruthy();
+
+    }))
+
+    fit("Asyncronous text example - plain Promise", fakeAsync(() => {
+
+        let test = false;
+
+        console.log("Creating promise");
+
+        Promise.resolve().then(() => {
+            console.log("Promise first then() evaluated sucessfully ");
+
+             test = true;
+            
+            return Promise.resolve();
+        })
+            .then(() => {
+
+            console.log("Promise second then() evaluated sucessfully ");
+
+        
+        });
+
+        flushMicrotasks();
+
+        console.log("Running test assertions");
+
+        expect(test).toBeTruthy();
+    }))
 
 })
